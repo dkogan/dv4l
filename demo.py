738d0c4e326a3ab251761dd57ee98ce8bb4569cd
Dima Kogan <dima@secretsauce.net>
2022-09-22 16:56:09 -0700

    Nicer demo: status bar shows the pixel under mouse, we flip the image

--- demo.py
+++ demo.py
@@ -7,10 +7,28 @@ from fltk import *
 from Fl_Gl_Image_Widget import Fl_Gl_Image_Widget
 import dv4l
 
+
+
+class Fl_Gl_Image_Widget_Derived(Fl_Gl_Image_Widget):
+    def handle(self, event):
+        if event == FL_MOVE:
+            try:
+                q = self.map_pixel_image_from_viewport( (Fl.event_x(),Fl.event_y()), )
+                status_widget.value(f"{q[0]:.1f},{q[1]:.1f}")
+            except:
+                status_widget.value("")
+            # fall through to let parent handlers run
+
+        return super().handle(event)
+
+
 window        = Fl_Window(800, 600, "video4linux camera stream")
-image_widget  = Fl_Gl_Image_Widget(0,0, 400,600)
-image_widget2 = Fl_Gl_Image_Widget(400,0, 400,600)
-window.resizable(window)
+group         = Fl_Group(0,0,800,580)
+image_widget  = Fl_Gl_Image_Widget_Derived(0,  0, 400,580)
+image_widget2 = Fl_Gl_Image_Widget_Derived(400,0, 400,580)
+group.end()
+status_widget = Fl_Output(0, 580, 800, 20)
+window.resizable(group)
 window.end()
 window.show()
 
@@ -19,8 +37,10 @@ c = dv4l.camera("/dev/video0")
 
 def update(fd):
     timestamp,image = c.get_frame()
-    image_widget .update_image(image_data = image)
-    image_widget2.update_image(image_data = 255-image)
+    image_widget .update_image(image_data = image, flip_x = True)
+    image_widget2.update_image(image_data = 255-image, flip_x = True, flip_y = True)
+
+
 Fl.add_fd(c.get_fd(),
           update)
 
